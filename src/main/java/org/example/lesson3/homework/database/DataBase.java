3dc14757f68172a8980fed254449d38ae3467359
klaes <klaes@mail.ru>
2023-10-22 23:28:36 +0300

    Семинар и дз к уроку от 23-10-17

--- src/main/java/org/example/lesson3/homework/database/DataBase.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson3.homework.database;
-
-public interface DataBase <T, Tid> {
-    boolean save(T data);
-    boolean remove(T data);
-    T getData(Tid id);
-}
--- src/main/java/org/example/lesson3/homework/nightclub/Person.java
+++
@@ -1,6 +0,0 @@
-package org.example.lesson3.homework.nightclub;
-
-public interface Person {
-    void doWork();
-    void haveRest();
-}
